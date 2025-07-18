95020edcadd65f3fd7219a55cef155d71a5a5a72
Richa Singh <richa.singhnxt213@gmail.com>
2025-07-18 19:34:03 +0530

    fix: rectify card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,11 +1,38 @@
 import "./Card.css";
-export default function Card(prop) {
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+
+export default function Card({ item }) {
   return (
     <div className="card">
-      <h4>{prop.item.title}</h4>
-      <div>
-        {prop.item.images.map((item) => {
-          return <img src={item} />;
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h4>{item.title}</h4>
+        <IoIosArrowDroprightCircle
+          style={{ color: "blue", width: "2rem", height: "2rem" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "auto auto",
+          rowGap: "5px",
+        }}
+      >
+        {item.images.map((item) => {
+          return (
+            <div className="image-container">
+              <img src={item.src} />
+              <div className="details">
+                <p>{item.title}</p>
+                <p>{item.other}</p>
+              </div>
+            </div>
+          );
         })}
       </div>
     </div>
