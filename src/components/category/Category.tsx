d32781b14ce8fedcb58b26987b68a1f2503163f1
Richa Singh <richa.singhnxt213@gmail.com>
2025-07-18 13:18:44 +0530

    feat: add category section with dummy data

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -1,3 +1,24 @@
+import "./Category.css";
+const dummyCategoryData = [
+  { name: "Grocery", image: "/assets/grocery.png" },
+  { name: "Mobiles", image: "/assets/mobiles.png" },
+  { name: "Fashion", image: "/assets/fashion.png" },
+  { name: "Electronics", image: "/assets/electronics.png" },
+  { name: "Home & Furniture", image: "/assets/furniture.webp" },
+  { name: "Appliances", image: "/assets/appliances.jpg" },
+  { name: "Flight Bookings", image: "/assets/flight.png" },
+  { name: "Beauty, Toys & More", image: "/assets/toys.webp" },
+  { name: "Two Wheelers", image: "/assets/two-wheeler.webp" },
+];
 export default function Category() {
-  return <div> Category</div>;
+  return (
+    <div className="category">
+      {dummyCategoryData.map((item) => (
+        <div>
+          <img src={item.image} />
+          <p>{item.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
